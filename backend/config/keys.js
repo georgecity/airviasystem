ff32a6e0aef8a004238346209b1beaa3fda6b553
georgecity <57677719+georgecity@users.noreply.github.com>
2020-04-03 14:34:09 +0100

    Update keys.js

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://dbAdmin:N0ureAb5s78ylx3J@cluster0-eupau.mongodb.net/test?retryWrites=true&w=majority" ,
+    mongoURI: "your db connection line" ,
     secretOrKey: "secret"
   };
